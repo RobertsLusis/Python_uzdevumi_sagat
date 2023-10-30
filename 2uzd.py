29af4d4f871d69caa9acf486153a91e9bd41311b
Roberts Lusis <roberts.lusis@gmail.com>
2023-10-30 15:47:59 +0200

    commit

--- 2uzd.py
+++ 2uzd.py
@@ -3,3 +3,15 @@ Iegūt ziņas, izmantojot rss no google.lv.
 
 """
 
+import feedparser
+#jānorāda RSS plūsmas URL
+rss_url="https://news.google.com/rss?hl=en-LV&gl=LV&ceid=LV:lv'"
+
+#lejupielādēt un izanalizēt RSS plūsmu
+feed=feedparser.parse(rss_url)
+
+#parādīt ziņu virsrakstus un saites
+for entry in feed.entries:
+    print("Virsraksts", entry.title)
+    print("Saite", entry.link)
+    print("\n")
